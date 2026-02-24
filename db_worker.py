3dd341952b8fda9d1e4ac558a5af606de30cab37
ilyashizuka <9930209@gmail.com>
2026-02-24 15:03:58 +0300

    Update db_worker.py

--- db_worker.py
+++ db_worker.py
@@ -1,14 +1,15 @@
 import pymysql
 
 DB_CONFIG = {
-    'host': 'mysql9.hostland.ru',
-    'port': 3306,  # Специфично для MySQL 5.7 на Hostland
-    'user': 'host1324224_botanik',
-    'password': '807bba4c', 
-    'database': 'host1324224_botanik',
+    'host': 'mysql9.hostland.ru',      # Внешний адрес Hostland
+    'port': 3306,                      # Порт для вашей версии MySQL 5.7
+    'user': 'host1324224_botanik',     # Пользователь БД
+    'password': '807bba4c',           # Пароль (тот, что вы писали выше)
+    'database': 'host1324224_botanik', # Имя базы данных
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
+
 
 def save_to_db(url, title, price, phone, content=""):
     try:
